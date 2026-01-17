1e92c3d13fe6bde5597bcbb7d421d41922f36469
serveny <dylan_lewis93@web.de>
2026-01-17 20:45:50 +0100

    deps update

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,9 +1,9 @@
-import streamDeck, { LogLevel } from "@elgato/streamdeck";
+import streamDeck from "@elgato/streamdeck";
 
 import { MatrixTile } from "./actions/tile";
 
 // We can enable "trace" logging so that all messages between the Stream Deck, and the plugin are recorded. When storing sensitive information
-streamDeck.logger.setLevel(LogLevel.INFO);
+streamDeck.logger.setLevel("info");
 
 // Register the increment action.
 streamDeck.actions.registerAction(new MatrixTile());
